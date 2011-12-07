b0afa0f14f825418444e82f4db03a28892eeaf89
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-12-06 20:20:40 -0600

    use my data files

--- run.sh
+++ run.sh
@@ -3,15 +3,12 @@ NODE_MEMORY_LIMIT=`echo "0.95 * $NODE_MEMORY / 1" | bc`
 #ulimit -v $NODE_MEMORY_LIMIT -m $NODE_MEMORY_LIMIT
 echo "memory limit: $NODE_MEMORY_LIMIT kilobytes"
 #ibrun ./reference_glm -f ./reference_glm.massive -v 2
-#-g /home/01722/tpatki/iplant/fstest/fstest/massiver.dat \
-#-g /scratch/01713/pbailey/massiver.dat \
-#-f /scratch/01713/pbailey/data_maize_nam/fixed.effects.nam.sorted.filtered.bin \
 ibrun ./reference_glm \
--f /scratch/01722/tpatki/fixed.effects.nam.sorted.filtered.bin \
+-f /scratch/01713/pbailey/data_maize_nam/fixed.effects.nam.sorted.filtered.bin \
 --num_fixed 26 \
--g /scratch/01722/tpatki/single.dat \
---num_geno 500000 \
--r /scratch/01722/tpatki/residuals.chr10.sorted.bin \
+-g /scratch/01713/pbailey/massiver.dat \
+--num_geno 1000000 \
+-r /scratch/01713/pbailey/data_maize_nam/residuals.chr10.sorted.bin \
 --num_r 4892 \
 -v2 \
 $1

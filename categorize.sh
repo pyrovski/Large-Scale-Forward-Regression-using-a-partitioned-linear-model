fd36fa74d17a6ef12ce22b5fc8ecb7834ad430ae
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-12-18 17:44:43 -0600

    update script to categorize runs.

--- categorize.sh
+++ categorize.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-find . -name 'run*.sh.o*' -print|grep -v '~'>.list
+find . -name 'id_0.log*' -print|grep -v '~'>.list
 #cpu=`eval "echo $list|grep cpu"`
 cat .list|xargs -I{} grep -Hci gpu {}>.gpucount
-cat .gpucount|egrep -v ':0'|cut -d':' -f1>.gpu
-cat .gpucount|grep ':0'|cut -d':' -f1>.cpu
+cat .gpucount|egrep -v ':0'|cut -d':' -f1|cut -d'/' -f2>.gpu
+cat .gpucount|grep ':0'|cut -d':' -f1|cut -d'/' -f2>.cpu
 
 echo id rank cpu_gpu iterations time comp>table
 
-cat .cpu|xargs -I{} ./entry.sh {} cpu >> table
-cat .gpu|xargs -I{} ./entry.sh {} gpu >> table
+#cat .cpu|xargs -I{} ./entry.sh {} cpu >> table
+#cat .gpu|xargs -I{} ./entry.sh {} gpu >> table

4730b62556f63ffaf7b027f0fe83bafd8552f71c
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-12-18 20:28:44 -0600

    fix table script

--- categorize.sh
+++ categorize.sh
@@ -5,7 +5,7 @@ cat .list|xargs -I{} grep -Hci gpu {}>.gpucount
 cat .gpucount|egrep -v ':0'|cut -d':' -f1|cut -d'/' -f2>.gpu
 cat .gpucount|grep ':0'|cut -d':' -f1|cut -d'/' -f2>.cpu
 
-echo id rank cpu_gpu iterations time comp>table
+echo id branch rank cpu_gpu iterations time comp SNPs_on_rank_0 >table
 
-#cat .cpu|xargs -I{} ./entry.sh {} cpu >> table
-#cat .gpu|xargs -I{} ./entry.sh {} gpu >> table
+cat .cpu|xargs -I{} ./entry.sh {} cpu >> table
+cat .gpu|xargs -I{} ./entry.sh {} gpu >> table

30d3d5a3fb87252e2a6c0197ac4bb971dc2fb8dc
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-12-19 17:45:08 -0600

    fix CPU submit script

--- submitCPU.sh
+++ submitCPU.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # $1 = number of CPU cores desired
 # $2 = number of nodes desired
-# wayness = number of cores per node = (1 if $1 == 1, else 2) = min(2, $1)
-wayness=`eval 'python -c "print min($1,2)"'`
+# $1 must be a >=1 multiple of $2
+# wayness = number of cores per node
+wayness=`eval 'python -c "print int($1/$2)"'`
 #cores=`eval 'python -c "from math import ceil; print int(ceil(4*$1/8.0)*8*3)"'`
 cores=`eval 'python -c "from math import ceil; print int($2 * 8)"'`
 #! todo create data dir for each run
 set -x
+
 name=`date +%Y_%m_%d_%H_%M_%S_%N`
 oldDir=`pwd`
 mkdir $name
@@ -18,5 +20,6 @@ uname -a >> info
 git log -n 1 --oneline >> info
 git branch | grep '*' >> info
 
+#echo $(echo -n $wayness)way $cores
 qsub -V -cwd -pe $(echo -n $wayness)way $cores -q normal -P hpc -l h_rt=0:12:00 -A TG-ASC100041 -o log -e errlog ../run.sh -c
 

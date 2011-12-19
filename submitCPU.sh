57d2fde8b3ab694f4319240ebddd23e5c81daac1
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-12-18 19:43:56 -0600

    add branch name to info

--- submitCPU.sh
+++ submitCPU.sh
@@ -16,6 +16,7 @@ hostname >> info
 echo $name >> info
 uname -a >> info
 git log -n 1 --oneline >> info
+git branch | grep '*' >> info
 
 qsub -V -cwd -pe $(echo -n $wayness)way $cores -q normal -P hpc -l h_rt=0:12:00 -A TG-ASC100041 -o log -e errlog ../run.sh -c
 

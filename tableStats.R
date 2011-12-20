33bf90e8cfd004b3f4a7bd59174484bc2f0388e6
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-12-20 00:51:50 -0700

    update

--- tableStats.R
+++ tableStats.R
@@ -21,21 +21,51 @@ a = read.table("filteredTable",header=T)
 #master_cpu_100000 = intersect(master_cpu_100000, which(a$SNPs_on_rank_0 == 100000))
 #summary(a$comp[master_cpu_100000])
 
-branchSel = which(a$branch == 'test_GPU_all')
-coreSel = which(a$cores.gpus == 1)
-sel = mergeSels(list(branchSel, coreSel))
+branchSelGPU_small = which(a$branch == 'test_GPU_all')
+branchSelGPU_large = which(a$branch == 'master')
+branchSelCPU = which(a$branch == 'master')
 
-conf = a$SNPs_on_rank_0[sel]
+gpuSel = which(a$cpu_gpu == 'gpu')
+cpuSel = which(a$cpu_gpu == 'cpu')
+
+coreSel = which(a$cores.gpus == 1)
+selGPU_small = mergeSels(list(branchSelGPU_small, gpuSel, coreSel))
+selGPU_large = mergeSels(list(branchSelGPU_large, gpuSel, coreSel))
+selCPU = mergeSels(list(branchSelCPU, cpuSel, coreSel))
+
+conf = a$SNPs_on_rank_0[selGPU_small]
 uconf = sort(unique(conf))
 
-# merge entries matching sel and uconf
-cat('branch cores/gpus count SNPs_on_rank_0 mean_time std_time\n')
-m = c()
-std = c()
+# merge entries matching selGPU_small and uconf
+cat('branch cores/gpus cpu_gpu count SNPs_on_rank_0 mean_time std_time\n')
+mGPU_small = c()
+mGPU_large = c()
+mCPU = c()
+
+stdGPU_small = c()
+stdGPU_large = c()
+stdCPU = c()
 for(i in 1:length(uconf)){
-  vals = a$comp[intersect(which(a$SNPs_on_rank_0 == uconf[i]), sel)]
-  m[i] = mean(vals)
-  std[i] = sd(vals)
+  valsGPU_small = a$comp[intersect(which(a$SNPs_on_rank_0 == uconf[i]), selGPU_small)]
+  mGPU_small[i] = mean(valsGPU_small)
+  stdGPU_small[i] = sd(valsGPU_small)
+
+  valsGPU_large = a$comp[intersect(which(a$SNPs_on_rank_0 == uconf[i]), selGPU_large)]
+  mGPU_large[i] = mean(valsGPU_large)
+  stdGPU_large[i] = sd(valsGPU_large)
+
+  valsCPU = a$comp[intersect(which(a$SNPs_on_rank_0 == uconf[i]), selCPU)]
+  mCPU[i] = mean(valsCPU)
+  stdCPU[i] = sd(valsCPU)
   
-  cat('test_GPU_all 1 ', uconf[i], ' ', m[i], ' ', std[i], '\n')
+  cat('test_GPU_all 1 gpu', uconf[i], ' ', mGPU_small[i], ' ', stdGPU_small[i], '\n')
+  cat('master 1 gpu', uconf[i], ' ', mGPU_large[i], ' ', stdGPU_large[i], '\n')
+  cat('master 1 cpu', uconf[i], ' ', mCPU[i], ' ', stdCPU[i], '\n')
 }
+
+yl = range(c(mGPU_small, mCPU))
+pdf('smallDataGPU.pdf')
+p = plot(uconf, mGPU_small, log='xy', xlab='log(SNP count)', ylab='log (computation time) (s)', main='computation time vs. SNP count', sub='small datasets', type='l', col='black', ylim=yl, lwd='2', lty=1)
+lines(uconf, mGPU_large, col='blue', lwd='2', lty=2)
+lines(uconf, mCPU, col='red', lwd='2', lty=3)
+legend(x='topleft', legend=c('gpu small', 'gpu large', 'cpu'), col=c('black', 'blue', 'red'), lwd=2, lty=1:3)

42e42e80e40a19363ae8a2ae6f6228fcc44ea196
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-12-19 18:54:37 -0600

    merge submit changes

--- submitCPU.sh
+++ submitCPU.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # $1 = number of CPU cores desired
 # $2 = number of nodes desired
-# wayness = number of cores per node = (1 if $1 == 1, else 2) = min(2, $1)
-wayness=`eval 'python -c "print min($1,2)"'`
+# $1 must be a >=1 multiple of $2
+# wayness = number of cores per node
+wayness=`eval 'python -c "print int($1/$2)"'`
 #cores=`eval 'python -c "from math import ceil; print int(ceil(4*$1/8.0)*8*3)"'`
 cores=`eval 'python -c "from math import ceil; print int($2 * 8)"'`
 #! todo create data dir for each run
@@ -18,6 +19,9 @@ echo $name >> info
 uname -a >> info
 git log -n 1 --oneline >> info
 git branch | grep '*' >> info
+echo 'cores: '$1 >> info
+echo 'nodes: '$2 >> info
 
+#echo $(echo -n $wayness)way $cores
 qsub -V -cwd -pe $(echo -n $wayness)way $cores -q normal -P hpc -l h_rt=0:12:00 -A TG-ASC100041 -o log -e errlog ../run.sh -c
 
--- submitGPU.sh
+++ submitGPU.sh
@@ -2,11 +2,12 @@
 # $1 = number of GPUs desired
 # $2 = number of nodes desired
 # number of cores to request = 8 * ceil(4 * $1 / 8)
-# wayness = number of cores per node = (1 if $1 == 1, else 2) = min(2, $1)
+# wayness = number of cores per node
 # for 1 core per socket
-cores=`eval 'python -c "from math import ceil; print int($2 * 8)"'`
-wayness=`eval 'python -c "print min($1,2)"'`
-#! todo create data dir for each run
+cores=`eval 'python -c "print int($2 * 8)"'`
+#wayness=`eval 'python -c "print min($1,2)"'`
+wayness=`eval 'python -c "print int($1/$2)"'`
+
 set -x
 
 name=`date +%Y_%m_%d_%H_%M_%S_%N`
@@ -19,5 +20,7 @@ echo $name >> info
 uname -a >> info
 git log -n 1 --oneline >> info
 git branch | grep '*' >> info
+echo 'cores: '$1 >> info
+echo 'nodes: '$2 >> info
 
 qsub -V -cwd -pe $(echo -n $wayness)way $cores -q normal -P gpgpu -l h_rt=0:12:00 -A TG-ASC100041 -o log -e errlog ../run.sh

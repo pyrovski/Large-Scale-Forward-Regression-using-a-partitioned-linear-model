3795072124fd11d245360b1ff6de40ece9de2fb2
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-12-21 16:50:29 -0600

    switch data file

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ echo "memory limit: $NODE_MEMORY_LIMIT kilobytes"
 ibrun ../reference_glm \
 -f /scratch/01713/pbailey/data_maize_nam/fixed.effects.nam.sorted.filtered.bin \
 --num_fixed 26 \
--g /scratch/01713/pbailey/massiver.dat \
+-g /scratch/01713/pbailey/4892x1M_double.dat \
 --num_geno 1000000 \
 -r /scratch/01713/pbailey/data_maize_nam/residuals.chr10.sorted.bin \
 --num_r 4892 \

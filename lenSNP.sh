9d3915226fe189f80caa3fcd368a8ac456d626f7
Peter Bailey <peter.eldridge.bailey@gmail.com>
2012-02-17 14:05:29 -0700

    update lenSNP plot

--- lenSNP.sh
+++ lenSNP.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env Rscript
 a = read.table('filteredTable', header=T)
+p = order(a$comp)
+a = a[p,]
 gpu = which(a$cpu_gpu == 'gpu')
 cpu = which(a$cpu_gpu == 'cpu')
 pdf('lenSNP.pdf')
-plot(a$SNP_len[cpu], a$comp[cpu]/a$SNPs_on_rank_0[cpu], ylim=c(0, max(a$comp[cpu]/a$SNPs_on_rank_0[cpu])))
-points(a$SNP_len[gpu], a$comp[gpu]/a$SNPs_on_rank_0[gpu])
+plot(a$SNP_len[cpu], a$comp[cpu]/a$SNPs_on_rank_0[cpu], 
+    ylim=c(0, max(a$comp[cpu]/a$SNPs_on_rank_0[cpu])), 
+    main='Time per SNP vs SNP length', 
+    xlab='SNP length', ylab='Time per SNP', type='l', col='red', lty=1, lwd=2,
+    sub='1M double precision SNPs')
+lines(a$SNP_len[gpu], a$comp[gpu]/a$SNPs_on_rank_0[gpu], col='black', lty=2, lwd=2)
+legend(x='topleft', legend=c('cpu', 'gpu'), col=c('red', 'black'), lwd=2, lty=1:2)
 dev.off()
